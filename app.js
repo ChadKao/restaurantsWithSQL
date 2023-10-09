e4e42350d57b6ec3b896582c5f360b9a4701914f
ChadKao <coolchad2000@gmail.com>
2023-10-09 18:14:27 +0800

    build: router init

--- app.js
+++ app.js
@@ -4,8 +4,43 @@ const port = 3000;
 
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.redirect('/restaurants')
 });
+
+app.get('/restaurants', (req, res) => {
+  res.send('restaurants')
+})
+
+app.get('/restaurants/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`restaurants/${id}`)
+})
+
+app.get('/restaurants/new', (req, res) => {
+  res.send('restaurants/new')
+})
+  
+app.post('/restaurants', (req, res) => {
+  res.send('new restaurants')
+})
+
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id;
+  res.send(`restaurants/${id}/edit`)
+})
+
+app.put('/restaurants/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`restaurants/${id}`)
+})
+
+
+app.delete('/restaurants/:id', (req, res) => {
+  const id = req.params.id;
+  res.send(`restaurants/${id}deleted`)
+})
+
+
 
 
 app.listen(port, () => {
